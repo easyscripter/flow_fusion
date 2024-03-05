bba50a4bbdf8b0250bbca856862629ef44f5b269
easyscripter <deniscs49@gmail.com>
2024-03-05 20:20:15 +0300

    clear project

--- src/render/config/sidebarItems.tsx
+++
@@ -1,9 +0,0 @@
-import { MenuProps } from "antd";
-import { NavLink } from "react-router-dom";
-
-export const sidebarItems: MenuProps["items"] = [
-  {
-    label: <NavLink to="/pomodoro">Pomodoro Timer</NavLink>,
-    key: "pomodoro",
-  },
-];
