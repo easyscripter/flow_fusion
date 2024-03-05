a02a55b2407b5a6c7c097e46280ebea630c54be1
Alexander Vinogradov <astronom235@gmail.com>
2024-03-06 01:43:39 +0300

    Test DI

--- lib/ui/pages/home_page/home_page.dart
+++ lib/ui/pages/home_page/home_page.dart
@@ -1,8 +1,23 @@
 import 'package:flow_fusion/gen/assets.gen.dart';
+import 'package:flow_fusion/model/datasources/local/prefs.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    final prefs = GetIt.instance.get<Prefs>();
+    prefs.buckets = 1;
+  }
 
   @override
   Widget build(BuildContext context) {
