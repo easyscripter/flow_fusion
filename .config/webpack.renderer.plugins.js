1ee055bf720365d6ae88a027a80f40e518d69422
Denis <deniscs49@gmail.com>
2024-03-05 20:21:37 +0300

    clear layout (#1)
    * init layout
    
    * clear project

--- .config/webpack.renderer.plugins.js
+++
@@ -1,4 +0,0 @@
-/* eslint @typescript-eslint/no-var-requires: "off" */
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-
-module.exports = [new ForkTsCheckerWebpackPlugin()];
--- src/bridges/types/bridges.d.ts
+++
@@ -1,8 +0,0 @@
-import { electronBridge, storeBridge } from "../main";
-
-declare global {
-  interface Window {
-    electron: typeof electronBridge;
-    store: typeof storeBridge;
-  }
-}
