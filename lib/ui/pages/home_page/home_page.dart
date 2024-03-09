036020c45dcde3726488aa92b6373eb8e8d3a162
Alexander Vinogradov <astronom235@gmail.com>
2024-03-10 00:43:51 +0300

    Add `mobx`, lock injectable version

--- lib/ui/pages/home_page/home_page.dart
+++ lib/ui/pages/home_page/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flow_fusion/gen/assets.gen.dart';
-import 'package:flow_fusion/model/datasources/database/dao/person_dao.dart';
-import 'package:flow_fusion/model/datasources/local/prefs.dart';
-import 'package:flow_fusion/model/entity/database/person.dart';
+import 'package:flow_fusion/ui/pages/home_page/home_page_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,23 +10,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _viewModel = HomePageViewModel();
+
   @override
   void initState() {
     super.initState();
-
-    (() async {
-      final prefs = GetIt.I.get<Prefs>();
-
-      prefs.buckets = 1;
-      print(prefs.buckets);
-
-      final personDao = GetIt.I.get<PersonDao>();
-
-      await personDao.clear();
-      await personDao.insertPerson(Person.optional(name: "Joe"));
-      await personDao.insertPerson(Person.optional(name: "Mike"));
-      personDao.findAllPeople().then((value) => print(value));
-    })();
+    _viewModel.init();
   }
 
   @override
