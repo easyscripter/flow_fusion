a96630ee61752404d93f40123947f154b34247b3
Alexander Vinogradov <alexander.vinogradov@raftds.com>
2025-08-29 12:11:54 +0300

    Migrate to freezed latest version

--- lib/model/entity/bucket.dart
+++ lib/model/entity/bucket.dart
@@ -5,11 +5,11 @@ part 'bucket.g.dart';
 
 // As an example
 @freezed
-class Bucket with _$Bucket {
-  factory Bucket({
+abstract class Bucket with _$Bucket {
+  const factory Bucket({
     required String name,
     required int volume,
   }) = _Bucket;
 
-  factory Bucket.fromJson(Map<String, dynamic> json) => _$BucketFromJson(json);
+  factory Bucket.fromJson(Map<String, Object?> json) => _$BucketFromJson(json);
 }
