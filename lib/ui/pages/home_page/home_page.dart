ba17ec30f0b11e3f3e607030b588d3c54f0ce600
denis.malyshev <denis.malyshev@raftds.com>
2025-10-19 18:37:51 +0300

    fix: remove sidebar_widget

--- lib/ui/pages/home_page/home_page.dart
+++ lib/ui/pages/home_page/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flow_fusion/ui/views/session_view/session_view.dart';
 import 'package:flow_fusion/ui/views/settings_view/settings_view.dart';
 import 'package:flow_fusion/ui/views/home_view/home_view.dart';
-import 'package:flow_fusion/ui/widgets/sidebar_widget.dart';
 import 'package:flow_fusion/ui/pages/home_page/home_page_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -80,17 +79,6 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-
-            // SidebarWidget(
-            //   menuIcons: const [
-            //     Icons.home_outlined, // Главная
-            //     Icons.schedule_outlined, // Сессии
-            //     Icons.settings_outlined, // Настройки
-            //   ],
-            //   menuLabels: const ['Главная', 'Сессии', 'Настройки'],
-            //   selectedIndex: _viewModel.selectedIndex,
-            //   onDestinationSelected: _viewModel.selectTab,
-            // ),
             Expanded(child: _buildContent()),
           ],
         ),
