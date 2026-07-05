45d576057d190b8de84878b5f8f53415df4089ce
easyscripter <deniscs49@gmail.com>
2026-07-05 19:46:07 +0300

    feat: add logs

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
+import 'dart:async';
+import 'dart:ui' show PlatformDispatcher;
+
 import 'package:flow_fusion/di/di.dart';
 import 'package:flow_fusion/ui/app/app.dart';
 import 'package:flow_fusion/ui/app/app_view_model.dart';
 import 'package:flow_fusion/ui/app/timer_alert_service.dart';
 import 'package:flow_fusion/ui/app/tray_service.dart';
 import 'package:flow_fusion/ui/constants/app_sizes.dart';
+import 'package:flow_fusion/utils/app_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:window_manager/window_manager.dart';
 
-Future<void> main() async {
+void main() {
+  runZonedGuarded(_startup, (error, stack) {
+    AppLogger.error('Zone', error, stack);
+  });
+}
+
+Future<void> _startup() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await AppLogger.init();
+
+  FlutterError.onError = (details) {
+    AppLogger.error('FlutterError', details.exception, details.stack);
+    FlutterError.presentError(details);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    AppLogger.error('PlatformDispatcher', error, stack);
+    return true;
+  };
+
   await configureDependencies();
   final timerAlertService = GetIt.I.get<TimerAlertService>();
   final trayService = GetIt.I.get<TrayService>();
